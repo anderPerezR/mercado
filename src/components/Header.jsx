8091b9ac03a866a1ae3ecc508cec51182165b40f
anderPerezR <andersonperez2206@gmail.com>
2023-07-30 11:36:56 -0500

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import logo from '../images/icono.png';
+
+
+const Header = () => {
+    return (
+      <div>
+        <footer>
+           <img src={logo} className='' />
+        </footer>
+      </div>
+          
+
+
+    )
+}
+
+export default Header
+
+
